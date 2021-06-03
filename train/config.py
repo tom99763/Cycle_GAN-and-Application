926c0abcc28182cba74d9407baf9ed6d4af628ec
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-06-03 10:02:47 +0800

    Update config.py

--- train/config.py
+++ train/config.py
@@ -5,7 +5,7 @@ class Config:
         self.val_X_path='../data/base_data/testX'
         self.val_Y_path='../data/base_data/testY'
         self.val_save_path='./generated_img'
-        self.batch_size=4
+        self.batch_size=1
         self.lr=2e-4
         self.lambda_identity=0.0
         self.lambda_cycle=10
