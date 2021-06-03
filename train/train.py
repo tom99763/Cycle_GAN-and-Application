92425b2496fbdfd185a5fc587f28cff665ccbad7
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-06-03 10:02:24 +0800

    Update train.py

--- train/train.py
+++ train/train.py
@@ -5,6 +5,8 @@ from model.G import Generator
 from preprocess import get_data
 from tqdm import tqdm
 import cv2
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 aug = tf.keras.Sequential([
     #tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
@@ -29,8 +31,11 @@ def flow(x,y,l1,l2,Dx,Dy,Gxy,Gyx,Dx_opt,Dy_opt,Gxy_opt,Gyx_opt):
         Dx_loss=l2(Dx(x,training=True),tf.ones_like(Dx(x,training=True)))+\
             l2(Dx(fake_x,training=True),tf.zeros_like(Dx(fake_x,training=True)))
 
+        Dx_loss*=params.lambda_dis
+
         Dy_loss = l2(Dy(y, training=True), tf.ones_like(Dy(y, training=True))) + \
                  l2(Dy(fake_y, training=True), tf.zeros_like(Dy(fake_y, training=True)))
+        Dy_loss*=params.lambda_dis
 
         #Gloss
         Gxy_loss=l2(fake_y,tf.ones_like(fake_y))
@@ -55,13 +60,14 @@ def flow(x,y,l1,l2,Dx,Dy,Gxy,Gyx,Dx_opt,Dy_opt,Gxy_opt,Gyx_opt):
 
     Gxy_grads = tape.gradient(total_Gxy_loss,Gxy.trainable_variables)
     Gyx_grads = tape.gradient(total_Gyx_loss,Gyx.trainable_variables)
-    Dx_grads = tape.gradient(Dx_loss*params.lambda_dis,Dx.trainable_variables)
-    Dy_grads = tape.gradient(Dy_loss*params.lambda_dis, Dy.trainable_variables)
+    Dx_grads = tape.gradient(Dx_loss,Dx.trainable_variables)
+    Dy_grads = tape.gradient(Dy_loss, Dy.trainable_variables)
 
     Gxy_opt.apply_gradients(zip(Gxy_grads,Gxy.trainable_variables))
     Gyx_opt.apply_gradients(zip(Gyx_grads, Gyx.trainable_variables))
     Dx_opt.apply_gradients(zip(Dx_grads, Dx.trainable_variables))
     Dy_opt.apply_gradients(zip(Dy_grads, Dy.trainable_variables))
+    return total_Gxy_loss,total_Gyx_loss,Dx_loss,Dy_loss
 
 
 def test(ds,Dx,Dy,Gxy,Gyx):
@@ -109,8 +115,8 @@ def main():
         for x, y in loop:
             x, y = x[0][:, 0, ...], y[0][:, 0, ...]
             x, y = aug(x), aug(y)
-            loss = flow(x, y, l1, l2, Dx, Dy, Gxy, Gyx, Dx_optim, Dy_optim, Gxy_optim, Gyx_optim)
-            loop.set_postfix(loss=f'loss:{loss}')
+            total_Gxy_loss,total_Gyx_loss,Dx_loss,Dy_loss = flow(x, y, l1, l2, Dx, Dy, Gxy, Gyx, Dx_optim, Dy_optim, Gxy_optim, Gyx_optim)
+            loop.set_postfix(loss=f'total_Gxy_loss:{total_Gxy_loss},total_Gyx_loss:{total_Gyx_loss},Dx_loss:{Dx_loss},Dy_loss:{Dy_loss}')
         ckpt_manager.save()
         if i//10==0:
             test(ds_val,Dx, Dy, Gxy, Gyx)
